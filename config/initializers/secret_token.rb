d3c3fe4d315061af0975adb656a713fbc3810258
Glauber Ramos <glauberamos@gmail.com>
2012-09-19 10:32:35 -0300

    adding initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horaprodutiva::Application.config.secret_token = '025753afa45eb16b3769a5b8aa9f41ebc5bd1364799ffbcd85021941fc9b1c6ec7c6330f21e15f6edce81be54d0ec22dfdfce6ad53701ec938f49da6f287d41c'
