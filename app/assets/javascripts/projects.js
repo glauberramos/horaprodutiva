3bf903bbcbc9c236490c5a4eec0c4643972db3b5
Glauber Ramos <glauberamos@gmail.com>
2012-10-04 18:22:06 -0300

    adding menu events

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('#menu-links').scrollspy();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 43) {
+      $('#menu').css('position', 'fixed').css('top', 0);
+      $('#menu').css('opacity', '0.9');
+      $('#project-information').css('margin-top', 43);
+    } else {
+      $('#menu').css('position', 'relative');
+      $('#menu').css('opacity', '1');
+      $('#project-information').css('margin-top', 0);
+    }
+  });
+
+  $(".anchor-link").click(function(event) {
+    event.preventDefault(event);
+    var session = $(this).attr('href');
+    var sessionPosition = $(session).offset().top;
+
+    $('body,html').animate({ scrollTop: sessionPosition }, 500, function() {
+        window.location.hash = session;
+    });
+  });
+});
