d554618129c7589e64b9d45fb68fd83c29bfa4b6
Glauber Ramos <glauberamos@gmail.com>
2012-10-04 16:08:03 -0300

    adding image drop location

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :final_date, :image_url, :name, :video_url, :tag_ids
   # has_many :microtasks
   has_and_belongs_to_many :tags
+
+  validates :tags, :presence => true
+  validates :name, :presence => true
+  validates :description, :presence => true
 end
