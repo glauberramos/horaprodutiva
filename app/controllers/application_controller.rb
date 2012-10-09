57e04bf16e9ce7a0d0e6c277530eb3fc070fc15e
Glauber Ramos <glauberamos@gmail.com>
2012-10-09 19:28:37 -0300

    adding auth to production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+   
   protect_from_forgery
+
+  private
+   def authenticate
+   	  if Rails.env.production?
+	    authenticate_or_request_with_http_basic do |id, password| 
+	      id == 'horaproduser' && password == 'horaprodpass'
+	    end
+  	  end
+   end
 end
